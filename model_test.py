219b6b55850f5c2414645da8fc8fa3e3a6d1365a
zhangtianhaogogogo <zth@umich.edu>
2018-06-10 22:08:23 -0500

    sb laoguo

---
+++ model_test.py
@@ -0,0 +1,170 @@
+import tensorflow as tf
+import numpy as np
+import random
+import sys
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def load_data_source(filename, batch_size):
+    classes = 18
+    depth = 3
+    input_size = 64
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label': tf.FixedLenFeature([1], tf.int64),
+                                           'image': tf.FixedLenFeature([], tf.string),
+                                       })
+    images = tf.decode_raw(features['image'], tf.int32)
+    images = tf.reshape(images, [depth, input_size, input_size])
+    images = tf.transpose(images, [1, 2, 0])
+    label = tf.cast(features['label'], tf.int32)
+
+    x_batch, y_batch = tf.train.shuffle_batch([images, label], batch_size=batch_size,
+                                              capacity=1000, min_after_dequeue=200, num_threads=4)
+    index = tf.expand_dims(tf.range(0, batch_size), 1)
+    concated = tf.concat([index, y_batch], axis=1)
+    y_batch = tf.cast(tf.sparse_to_dense(concated, [batch_size, classes], 1.0, 0.0), dtype=tf.float32)
+
+    return x_batch, y_batch
+
+
+def cnn_model_fn():
+
+    # tag of train of test
+    tag = tf.placeholder(tf.bool, name = 'tag')
+    # Input Layer
+    x = tf.placeholder("float", [None , 64, 64, 3], name="x")
+
+    x_norm = tf.layers.batch_normalization(x, training = tag)
+    # Convolutional Layer #1
+    #32
+    conv1_1 = tf.layers.conv2d(
+        inputs=x_norm,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        name="first",
+        activation=tf.nn.relu)
+
+    conv1_1_norm = tf.layers.batch_normalization(conv1_1, training = tag)
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1_1_norm, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #2
+    #64
+    conv2_1 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        name="second",
+        activation=tf.nn.relu
+    )
+
+    conv2_1_norm = tf.layers.batch_normalization(conv2_1, training=tag)
+    # Pooling Layer #2
+    pool2 = tf.layers.max_pooling2d(inputs=conv2_1_norm, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #3
+    #128
+    conv3_1 = tf.layers.conv2d(
+        inputs=pool2,
+        filters=128,
+        kernel_size=[5, 5],
+        padding="same",
+        name="third",
+        activation=tf.nn.relu
+    )
+
+    conv3_1_norm = tf.layers.batch_normalization(conv3_1, training=tag)
+    # Pooling Layer #3
+    pool3 = tf.layers.average_pooling2d(inputs=conv3_1_norm, pool_size=[2, 2], strides=2)
+
+    pool_flat = tf.reshape(pool3, [-1, 8 * 8 * 128])
+
+    dense = tf.layers.dense(inputs=pool_flat, units=1024, activation=tf.nn.relu)
+
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.2)
+
+    logits = tf.layers.dense(inputs=dropout, units=18)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+
+    y = tf.placeholder("float", [None, 18], name="y")
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=logits)
+    tf.summary.scalar('loss', loss)
+
+    lr = tf.train.exponential_decay(0.001, 5000, 200, 0.9)
+    # optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
+    optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+
+    correct_prediction = tf.equal(tf.argmax(logits, axis=1), tf.argmax(y, axis=1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    tf.summary.scalar('accuracy', accuracy)
+    merged = tf.summary.merge_all()
+
+    return x, y, tag, train_op, accuracy, loss, merged
+
+def save(sess):
+    saver = tf.train.Saver()
+    save_path = saver.save(sess, "./model/model.ckpt")
+    print("Model saved in file: %s" % save_path)
+
+def TrainAndTest():
+    # Load training and eval data
+    # mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    batch_size = 32
+    test_step = 10
+    x_train, y_train = load_data_source("train.tfrecord", batch_size)
+    x_test, y_test = load_data_source("test.tfrecord", batch_size)
+    training_iters = 4000
+    x, y, tag, train_op, accuracy, loss, merged = cnn_model_fn()
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        sess.run(init)
+        print("===== Start Training! =====")
+        train_writer = tf.summary.FileWriter('./train_v1', sess.graph)
+        generation_error = []
+        step = 1
+        while step <= training_iters:
+            batch_xs, batch_ys= sess.run([x_train, y_train])
+            sess.run(train_op, feed_dict={x: batch_xs, y: batch_ys, tag: True})
+            if step % test_step == 0:
+                summary, train_loss, train_acc = sess.run([merged, loss, accuracy],
+                                              feed_dict={x: batch_xs, y: batch_ys, tag: True})
+                train_writer.add_summary(summary, step)
+                print("Iter " + str(step) + ":\nTrain Loss = " + "{:.6f}".format(
+                    train_loss) + ", Train Accuracy = " + "{:.5f}".format(train_acc))
+                test_data, test_label = sess.run([x_test, y_test])
+                test_loss, test_acc = sess.run([loss, accuracy], feed_dict={x: test_data, y: test_label, tag: False})
+                print("Test Loss = " + "{:.6f}".format(test_loss) + ", Test Accuracy = " + "{:.5f}".format(test_acc))
+                print("Gen Error = " + "{:.6f}".format(test_loss - train_loss))
+                generation_error.append(test_loss - loss)
+            step += 1
+        print("Optimization Finished!")
+        np.save('generation_v1.npy', np.array(generation_error))
+        train_writer.close()
+        coord.request_stop()
+        coord.join(threads)
+        save(sess)
+
+
+if __name__=="__main__":
+    TrainAndTest()
--- test.py
+++ test.py
@@ -1,35 +1,33 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import tensorflow as tf
-import numpy as np
 import random
 import sys
-import shiqiang as sq
+import numpy as np
+from numpy import linalg as LA
 
-#ASSET_DIR = '../asset'
+# ASSET_DIR = '../asset'
+#ASSET_DIR = './the-simpsons-characters-dataset'
 ASSET_DIR = '/Users/tianzhang/Documents/NU/EECS349/project/asset'
 ANNOTATION_FILE = ASSET_DIR + '/annotation.txt'
 
-new_max = 127
-new_min = -128
+NORM_FACTOR = 128
+OFFSET = 0.5
 
 annotation = open(ANNOTATION_FILE, "r")
-NUMBER = 1000
+NUMBER = 2000
 PIC_SIZE = 64
-map_characters = {'abraham_grampa_simpson\n':0, 'apu_nahasapeemapetilon\n':1, 'bart_simpson\n':2,
-        'charles_montgomery_burns\n':3, 'chief_wiggum\n':4, 'comic_book_guy\n':5, 'edna_krabappel\n':6,
-        'homer_simpson\n':7, 'kent_brockman\n':8, 'krusty_the_clown\n':9, 'lisa_simpson\n':10,
-        'marge_simpson\n':11, 'milhouse_van_houten\n':12, 'moe_szyslak\n':13,
-        'ned_flanders\n':14, 'nelson_muntz\n':15, 'principal_skinner\n':16, 'sideshow_bob\n':17}
+map_characters = {'abraham_grampa_simpson\n': 0, 'apu_nahasapeemapetilon\n': 1, 'bart_simpson\n': 2,
+                  'charles_montgomery_burns\n': 3, 'chief_wiggum\n': 4, 'comic_book_guy\n': 5, 'edna_krabappel\n': 6,
+                  'homer_simpson\n': 7, 'kent_brockman\n': 8, 'krusty_the_clown\n': 9, 'lisa_simpson\n': 10,
+                  'marge_simpson\n': 11, 'milhouse_van_houten\n': 12, 'moe_szyslak\n': 13,
+                  'ned_flanders\n': 14, 'nelson_muntz\n': 15, 'principal_skinner\n': 16, 'sideshow_bob\n': 17}
 
-def _parse_function (filename):
+def _parse_function(filename):
     image_string = tf.read_file(filename)
     image_decoded = tf.image.decode_jpeg(image_string)
     # image_resized = tf.image.resize_images(image_decoded, [100, 100], method=1)
     return image_decoded
 
-#return a list-of-pic_dir
-#return tag: transfer tag from characters to int
+
 def pic_dir_list():
     pic_dir = []
     _x1 = []
@@ -40,7 +38,7 @@ def pic_dir_list():
     _tag = []
     first = True
     for line in annotation:
-        if(first):
+        if (first):
             first = False
             continue
         [image_dir, x1, y1, x2, y2, tag] = line.split(',')
@@ -56,7 +54,8 @@ def pic_dir_list():
         _tag.append(map_characters[i])
     return pic_dir, _x1, _y1, _x2, _y2, _tag
 
-#return 500 randomly choosen pictures from pic_list and relavant parameters
+
+# return 500 randomly choosen pictures from pic_list and relavant parameters
 def random_choose_pics(pic_list, x1, y1, x2, y2, tag):
     pic_choosen = []
     _x1 = []
@@ -64,7 +63,7 @@ def random_choose_pics(pic_list, x1, y1, x2, y2, tag):
     _x2 = []
     _y2 = []
     _tag = []
-    num_choosen = random.sample(range(1, len(pic_list)+1), NUMBER)
+    num_choosen = random.sample(range(1, len(pic_list)), NUMBER)
     for i in num_choosen:
         pic_choosen.append(pic_list[i])
         _x1.append(x1[i])
@@ -74,101 +73,59 @@ def random_choose_pics(pic_list, x1, y1, x2, y2, tag):
         _tag.append(tag[i])
     return pic_choosen, _x1, _y1, _x2, _y2, _tag
 
-#return array of resized pictures
-#return array of tag of pictures
-def loadImage():
+def norm(v):
+    if LA.norm(v) == 0:
+       return 1
+    norm = LA.norm(v)
+    return norm
 
-    pic_list = []
-    pic_choosen = []
-    x1 =[]
-    y1 = []
-    x2 = []
-    y2 = []
-    tag_ = []
-    tag_train = []
-    tag_test = []
-    pic_res = []
-    test = []
+if __name__ == "__main__":
     pic_list, x1, y1, x2, y2, tag_ = pic_dir_list()
-
-    #randomly choose 500 pictures from pic_list
-    pic_choosen, x1, y1, x2, y2, tag_  = random_choose_pics(pic_list, x1, y1, x2, y2, tag_)
+    # randomly choose 500 pictures from pic_list
+    pic_choosen, x1, y1, x2, y2, tag_ = random_choose_pics(pic_list, x1, y1, x2, y2, tag_)
 
     # check error
-    if len(pic_choosen) != NUMBER or len(x1) != NUMBER or len(y1) != NUMBER or len(x2) != NUMBER or len(y2) != NUMBER or len(tag_) != NUMBER:
+    if len(pic_choosen) != NUMBER or len(x1) != NUMBER or len(y1) != NUMBER or len(x2) != NUMBER or len(
+            y2) != NUMBER or len(tag_) != NUMBER:
         print >> sys.stderr, 'wrong index!'
 
     print "len of pic_choosen is:", len(pic_choosen)
-    #here for use if statement for debug use:
+
+    train_writer = tf.python_io.TFRecordWriter("train.tfrecord")
+    test_writer = tf.python_io.TFRecordWriter("test.tfrecord")
+    sess = tf.Session()
     for i in range(0, len(pic_choosen)):
-        with tf.Session() as sess:
-            _image = []
-            image = _parse_function(pic_choosen[i])
-            if int(y2[i]) - int(y1[i]) <= 0 or int(x2[i]) - int(x1[i]) <= 0:
-                continue
-            image = tf.image.crop_to_bounding_box(image, int(y1[i]), int(x1[i]), int(y2[i]) - int(y1[i]), int(x2[i]) - int(x1[i]))
-            image_resized = tf.image.resize_images(image, [PIC_SIZE, PIC_SIZE], method=1)
+        _image = []
+        image = _parse_function(pic_choosen[i])
+        if int(y2[i]) - int(y1[i]) <= 0 or int(x2[i]) - int(x1[i]) <= 0:
+            continue
+        image = tf.image.crop_to_bounding_box(image, int(y1[i]), int(x1[i]), int(y2[i]) - int(y1[i]),
+                                              int(x2[i]) - int(x1[i]))
+        image_resized = tf.image.resize_images(image, [PIC_SIZE, PIC_SIZE], method=1)
 
-            _image = np.array(image_resized.eval(session=sess))
+        # #do the normalizatin for each image:
+        # each_image = np.array(image_resized.eval(session=sess), dtype=np.int32)
+        # #R
+        # each_image[:,:,0] = ((each_image[:,:,0] / norm(each_image[:,:,0])) - OFFSET) * NORM_FACTOR
+        # #G
+        # each_image[:,:,1] = ((each_image[:,:,1] / norm(each_image[:,:,1])) - OFFSET) * NORM_FACTOR
+        # #B
+        # each_image[:,:,2] = ((each_image[:,:,2] / norm(each_image[:,:,2])) - OFFSET) * NORM_FACTOR
+        #each_image = each_image.flatten()
 
-            if i % 10 == 0 or i % 10 == 5 or i % 10 == 8:
-                test.append(_image)
-                tag_test.append(tag_[i])
-            else:
-                pic_res.append(_image)
-                tag_train.append(tag_[i])
-            #plt.imshow(image.eval(session=sess))
-            #plt.show()
-    tag_num = len(tag_train)
-    tag_train = np.array(tag_train)
-    tag_train.shape = (1, tag_num)
+        each_image = np.array(image_resized.eval(session=sess), dtype=np.int32).flatten()
 
-    tag_num = len(tag_test)
-    tag_test = np.array(tag_test)
-    tag_test.shape = (1, tag_num)
+        feature = each_image.tostring()
+        label = tag_[i]
+        example = tf.train.Example(
+            features=tf.train.Features(
+                feature={'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label])),
+                         'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[feature]))
+                         }))
+        if i % 10 == 0 or i % 10 == 5 or i % 10 == 8:
+            test_writer.write(example.SerializeToString())
+        else:
+            train_writer.write(example.SerializeToString())
 
-    pic_res = np.array(pic_res)
-    test = np.array(test)
-
-    return pic_res, test, tag_train.T, tag_test.T
-
-#input
-def Normalization(a):
-    #rememeber the outer dimension of array
-    o_dimension = a.shape[0]
-    for i in range(o_dimension):
-        i_dimension = a[i].shape
-        temp1 = a[i].flatten()
-        temp2 = sorted(a[i].flatten())
-        min = temp2[0]
-        max = temp2[-1]
-        idx = 0
-        while(idx < len(temp1)):
-            temp1[idx] = (temp1[idx]-min)*(new_max - new_min) / (max - min) + new_min
-            idx += 1
-        temp1.shape = i_dimension
-        a[i] = temp1
-    return a
-
-if __name__ == '__main__':
-    picsdata = []
-    testdata = []
-    tag_train = []
-    tag_test = []
-
-    picsdata, testdata, tag_train, tag_test = loadImage()
-    picsdata = Normalization(picsdata)
-    testdata = Normalization(testdata)
-    tag_train = Normalization(tag_train)
-    tag_test = Normalization(tag_test)
-
-    print("the shape of pic data is: ")
-    print(picsdata.shape)
-    print("the shape of test data is: ")
-    print(testdata.shape)
-    print("the shape of tag_train data is: ")
-    print(tag_train.shape)
-    print("the shape of tag_test data is: ")
-    print(tag_test.shape)
-
-    sq.TrainAndTest(picsdata, testdata, tag_train.flatten(), tag_test.flatten())
+    train_writer.close()
+    test_writer.close()
